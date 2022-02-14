ab89911ccf078d6bb9d541606a195858028fd6a7
Anita <tuzson.anita@gmail.com>
2022-02-14 15:15:21 +0200

    Initial commit

---
+++ Assets/Scripts/BurnZone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BurnZone : MonoBehaviour, IDropHandler
+{
+    public AudioSource burnAudio = null;
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log("Card dropped on burn zone");
+
+        GameObject obj= eventData.pointerDrag;
+        Card card = obj.GetComponent<Card>();
+        if(card!=null)
+        {
+            PlayBurnSound();
+            GameController.instance.playersHand.RemoveCard(card);
+            GameController.instance.NextPlayersTurn();
+        }
+    }
+
+    internal void PlayBurnSound()
+    {
+        burnAudio.Play();
+    }
+}
